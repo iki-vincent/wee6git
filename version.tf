5d7394edfe6afb625d3f9f661e6013406931c1db
iki-vincent <yikivincent421@gmail.com>
2024-06-08 14:46:23 -0400

    new2

--- version.tf
+++ version.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws ={
         source = "hashicorp/aws"
-        version = "4.60.0"
+        version = "4.60.2"
 
     }
   }
 }
 provider "aws" {
-    region = "us-east-1b"
+    region = "us-east-1a"
 }
